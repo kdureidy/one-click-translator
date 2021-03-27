eb5c113429f5c53200f6437e53e2050a6dd48ff9
Khalil Dureidy <kdureidy@gmail.com>
2021-03-27 20:26:06 +0300

    fix translation resource

--- src/main/java/com/kdureidy/oneclick/web/rest/TranslationResource.java
+++ src/main/java/com/kdureidy/oneclick/web/rest/TranslationResource.java
@@ -1,15 +1,18 @@
 package com.kdureidy.oneclick.web.rest;
 
-import com.codahale.metrics.annotation.Timed;
+import io.micrometer.core.annotation.Timed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class TranslationResource {
+
+    private static final Logger log = LoggerFactory.getLogger(TranslationResource.class);
 
     public TranslationResource() {}
 
